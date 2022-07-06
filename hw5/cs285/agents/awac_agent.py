7794024e9f8b05a759e1f7b2cc4cf3cfe7b3d95b
hdvvip <nicknguyenhdv@gmail.com>
2022-07-06 07:39:37 -0400

    submit hw5

---
+++ hw5/cs285/agents/awac_agent.py
@@ -0,0 +1,189 @@
+from collections import OrderedDict
+
+from cs285.critics.dqn_critic import DQNCritic
+from cs285.critics.cql_critic import CQLCritic
+from cs285.infrastructure.replay_buffer import ReplayBuffer
+from cs285.infrastructure.utils import *
+from cs285.infrastructure import pytorch_util as ptu
+from cs285.policies.argmax_policy import ArgMaxPolicy
+from cs285.infrastructure.dqn_utils import MemoryOptimizedReplayBuffer
+from cs285.exploration.rnd_model import RNDModel
+from .dqn_agent import DQNAgent
+from cs285.policies.MLP_policy import MLPPolicyAWAC
+import numpy as np
+import torch
+
+
+class AWACAgent(DQNAgent):
+    def __init__(self, env, agent_params, normalize_rnd=True, rnd_gamma=0.99):
+        super(AWACAgent, self).__init__(env, agent_params)
+        
+        self.replay_buffer = MemoryOptimizedReplayBuffer(100000, 1, float_obs=True)
+        self.num_exploration_steps = agent_params['num_exploration_steps']
+        self.offline_exploitation = agent_params['offline_exploitation']
+
+        self.exploitation_critic = DQNCritic(agent_params, self.optimizer_spec)
+        self.exploration_critic = DQNCritic(agent_params, self.optimizer_spec)
+        
+        self.exploration_model = RNDModel(agent_params, self.optimizer_spec)
+        self.explore_weight_schedule = agent_params['explore_weight_schedule']
+        self.exploit_weight_schedule = agent_params['exploit_weight_schedule']
+        
+        self.use_boltzmann = agent_params['use_boltzmann']
+        self.actor = ArgMaxPolicy(self.exploitation_critic)
+        self.eval_policy = self.awac_actor = MLPPolicyAWAC(
+            self.agent_params['ac_dim'],
+            self.agent_params['ob_dim'],
+            self.agent_params['n_layers'],
+            self.agent_params['size'],
+            self.agent_params['discrete'],
+            self.agent_params['learning_rate'],
+            self.agent_params['awac_lambda'],
+        )
+
+        self.exploit_rew_shift = agent_params['exploit_rew_shift']
+        self.exploit_rew_scale = agent_params['exploit_rew_scale']
+        self.eps = agent_params['eps']
+
+        self.running_rnd_rew_std = 1
+        self.normalize_rnd = normalize_rnd
+        self.rnd_gamma = rnd_gamma
+
+    def get_qvals(self, critic, obs, action):
+        # get q-value for a given critic, obs, and action
+        qa_t_values = critic.q_net(obs)
+        q_value = torch.gather(qa_t_values, 1, action.unsqueeze(1)).squeeze(1)
+        return q_value
+
+    def estimate_advantage(self, ob_no, ac_na, re_n, next_ob_no, terminal_n, n_actions=10):
+        # TODO convert to torch tensors
+        ob_no = ptu.from_numpy(ob_no)
+        ac_na = ptu.from_numpy(ac_na).to(torch.long)
+        next_ob_no = ptu.from_numpy(next_ob_no)
+        re_n = ptu.from_numpy(re_n)
+        terminal_n = ptu.from_numpy(terminal_n)
+
+        # vals = []
+        # # TODO Calculate Value Function Estimate given current observation
+        # # You may find it helpful to utilze get_qvals defined above
+        # dist = None
+        # if self.agent_params['discrete']:
+        #     for i in range(self.agent_params['ac_dim']):
+        #         pass
+        # else:
+        #     for _ in range(n_actions):
+        #         pass
+        # v_pi = None
+        # # TODO Calculate Q-Values
+        # q_vals = 
+        # # TODO Calculate the Advantage
+
+        qa_t_values = self.exploitation_critic.q_net(ob_no)
+        q_t_values = torch.gather(qa_t_values, 1, ac_na.unsqueeze(1)).squeeze(1)
+        qa_tp1_values = self.exploitation_critic.q_net_target(next_ob_no)
+
+        next_actions = self.exploitation_critic.q_net(next_ob_no).argmax(dim=1)
+        q_tp1 = torch.gather(qa_tp1_values, 1, next_actions.unsqueeze(1)).squeeze(1)
+
+        adv_n = re_n + self.rnd_gamma * q_tp1 * (1 - terminal_n) - q_t_values
+
+        if self.normalize_rnd:
+            adv_n = normalize(adv_n, adv_n.mean(), adv_n.std())        
+        
+        return adv_n
+
+    def train(self, ob_no, ac_na, re_n, next_ob_no, terminal_n):
+        log = {}
+
+        if self.t > self.num_exploration_steps:
+            self.actor.set_critic(self.exploitation_critic)
+            self.actor.use_boltzmann = False
+
+        if (self.t > self.learning_starts
+                and self.t % self.learning_freq == 0
+                and self.replay_buffer.can_sample(self.batch_size)
+        ):
+            # TODO: Get Reward Weights
+            # Get the current explore reward weight and exploit reward weight
+            explore_weight = self.explore_weight_schedule.value(self.t)
+            exploit_weight = self.exploit_weight_schedule.value(self.t)
+
+            # TODO: Run Exploration Model #
+            # Evaluate the exploration model on s' to get the exploration bonus
+            # HINT: Normalize the exploration bonus, as RND values vary highly in magnitudeelse:
+            expl_bonus = self.exploration_model.forward_np(next_ob_no)
+            expl_bonus = normalize(expl_bonus, expl_bonus.mean(), expl_bonus.std())
+
+            # TODO: Reward Calculations #
+            # Calculate mixed rewards, which will be passed into the exploration critic
+            # HINT: See doc for definition of mixed_reward
+            mixed_reward = explore_weight*expl_bonus + exploit_weight*re_n
+            
+            # TODO: Calculate the environment reward
+            # HINT: For part 1, env_reward is just 're_n'
+            #       After this, env_reward is 're_n' shifted by self.exploit_rew_shift,
+            #       and scaled by self.exploit_rew_scale
+            env_reward = (re_n + self.exploit_rew_shift) * self.exploit_rew_scale
+
+            # TODO: Update Critics And Exploration Model #
+            # 1): Update the exploration model (based off s')
+            # 2): Update the exploration critic (based off mixed_reward)
+            # 3): Update the exploitation critic (based off env_reward)
+            expl_model_loss = self.exploration_model.update(next_ob_no)
+            exploration_critic_loss = self.exploration_critic.update(ob_no, ac_na, next_ob_no, 
+                                                                    mixed_reward, terminal_n)
+            exploitation_critic_loss = self.exploitation_critic.update(ob_no, ac_na, next_ob_no, 
+                                                                       env_reward, terminal_n)
+
+            # TODO: update actor
+            # 1): Estimate the advantage
+            # 2): Calculate the awac actor loss
+            adv_n = self.estimate_advantage(ob_no, ac_na, re_n, next_ob_no, terminal_n)
+            actor_loss = self.awac_actor.update(ob_no, ac_na, adv_n)
+            
+            # TODO: Update Target Networks #
+            if self.num_param_updates % self.target_update_freq == 0:
+                #  Update the exploitation and exploration target networks
+                self.exploration_critic.update_target_network()
+                self.exploitation_critic.update_target_network()
+
+            # Logging #
+            log['Exploration Critic Loss'] = exploration_critic_loss['Training Loss']
+            log['Exploitation Critic Loss'] = exploitation_critic_loss['Training Loss']
+            log['Exploration Model Loss'] = expl_model_loss
+
+            # Uncomment these lines after completing awac
+            log['Actor Loss'] = actor_loss
+
+            self.num_param_updates += 1
+
+        self.t += 1
+        return log
+
+
+    def step_env(self):
+        """
+            Step the env and store the transition
+            At the end of this block of code, the simulator should have been
+            advanced one step, and the replay buffer should contain one more transition.
+            Note that self.last_obs must always point to the new latest observation.
+        """
+        if (not self.offline_exploitation) or (self.t <= self.num_exploration_steps):
+            self.replay_buffer_idx = self.replay_buffer.store_frame(self.last_obs)
+
+        perform_random_action = np.random.random() < self.eps or self.t < self.learning_starts
+
+        if perform_random_action:
+            action = self.env.action_space.sample()
+        else:
+            processed = self.replay_buffer.encode_recent_observation()
+            action = self.actor.get_action(processed)
+
+        next_obs, reward, done, info = self.env.step(action)
+        self.last_obs = next_obs.copy()
+
+        if (not self.offline_exploitation) or (self.t <= self.num_exploration_steps):
+            self.replay_buffer.store_effect(self.replay_buffer_idx, action, reward, done)
+
+        if done:
+            self.last_obs = self.env.reset()
